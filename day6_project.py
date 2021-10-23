97e9ce0c594e52cfbb0a6818ebadcce09ff237d9
Stephe <72951916+StephaniePar@users.noreply.github.com>
2021-10-22 23:01:03 -0400

    Create day6_project.py

---
+++ day6_project.py
@@ -0,0 +1,14 @@
+#My solution for Maze Go to: https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Maze&url=worlds%2Ftutorial_en%2Fmaze1.json
+# My Maze Project Solution:
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+while not at_goal():
+    if front_is_clear():
+        move()
+    elif wall_in_front() and right_is_clear():
+        turn_right()
+    else:
+        turn_left()
